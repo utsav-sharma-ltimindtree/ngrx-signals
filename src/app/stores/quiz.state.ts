ec2a69ab4ea5652eb4ba8c307806a8d1580391bc
utsav-sharma-ltimindtree <utsav.sharma@ltimindtree.com>
2025-12-31 15:17:33 +0530

    added withHooks in store

--- src/app/stores/quiz.state.ts
+++ src/app/stores/quiz.state.ts
@@ -1,8 +1,16 @@
-import { computed } from "@angular/core";
-import { QUESTIONS } from "../data/questions";
-import { Question } from "../models/question.model";
-import { signalStore, withState, withComputed, withMethods, patchState } from "@ngrx/signals";
-import { addAnswerUpdater, resetUpdater } from "./quiz.updater";
+import { computed, effect } from '@angular/core';
+import { QUESTIONS } from '../data/questions';
+import { Question } from '../models/question.model';
+import {
+  signalStore,
+  withState,
+  withComputed,
+  withMethods,
+  patchState,
+  withHooks,
+  getState,
+} from '@ngrx/signals';
+import { addAnswerUpdater, resetUpdater } from './quiz.updater';
 
 export interface IQuizState {
   questions: Question[];
@@ -15,21 +23,35 @@ export const initialQuizState: IQuizState = {
 };
 
 export const QuizStore = signalStore(
-    { providedIn: 'root' },
-    withState<IQuizState>(initialQuizState),
-    withComputed(({ questions, answers }) => {
-        const currentQuestionIndex = computed(() => answers().length);
-        const currentQuestion = computed(() => {
-          let cqi = currentQuestionIndex();
-          cqi = cqi === questions().length ? cqi - 1 : cqi;
-          return questions()[cqi];
-        });
-        const isDone = computed(() => currentQuestionIndex() === questions().length)
+  { providedIn: 'root' },
+  withState<IQuizState>(initialQuizState),
+  withComputed(({ questions, answers }) => {
+    const currentQuestionIndex = computed(() => answers().length);
+    const currentQuestion = computed(() => {
+      let cqi = currentQuestionIndex();
+      cqi = cqi === questions().length ? cqi - 1 : cqi;
+      return questions()[cqi];
+    });
+    const isDone = computed(() => currentQuestionIndex() === questions().length);
 
-        return { currentQuestionIndex, currentQuestion, isDone }
-    }),
-    withMethods(store => ({
-      addAnswer: (index: number) => patchState(store, addAnswerUpdater(index)),
-      reset: () => patchState(store, resetUpdater())
-    }))
+    return { currentQuestionIndex, currentQuestion, isDone };
+  }),
+  withMethods((store) => ({
+    addAnswer: (index: number) => patchState(store, addAnswerUpdater(index)),
+    reset: () => patchState(store, resetUpdater()),
+  })),
+  withHooks({
+    onInit: (store) => {
+      const json = localStorage.getItem('quiz');
+      if (json) {
+        const storePatch = JSON.parse(json);
+        patchState(store, storePatch);
+      }
+      effect(() => {
+        const state = getState(store);
+        const json = JSON.stringify(state);
+        localStorage.setItem('quiz', json);
+      });
+    },
+  })
 );
